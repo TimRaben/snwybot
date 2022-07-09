b52fdc476ae1e09cef9d818ac54448993a11a237
TimRaben <timraben07@gmail.com>
2022-07-09 10:45:50 +0200

    i
    i

--- commands/nanoleaf.js
+++ commands/nanoleaf.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     let role = message.guild.roles.cache.some(roles => role.name === 'Nanoleaf')
-    message.member.role.add(role)
+    message.member.roles.add(role)
     
     message.channel.send(`${Member.displayName} heeft succesvol de NanoLeaf XP actie geclaimed!`)
     }
